3b70af66e14dcad80f108cea58c0d97ed659db5a
Dmitry Sergeevich <efos.ds@gmail.com>
2024-11-14 01:18:59 +0700

    add utils

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.scss';
+import { Routes, Route } from "react-router-dom";
+import Header from './components/Header/Header';
+import MainPage from './pages/MainPage/MainPage';
 
 function App() {
   return (
-    <div className="App">
-      qwe
-      <div>123</div>
-    </div>
+    <>
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<MainPage />} />
+      </Routes>
+    </>
   );
 }
 
